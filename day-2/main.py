ac15ce045ae1aa337496a0c5106bf88fe451938b
Robotosail <d.rojugbokan>
2022-06-20 21:59:16 -0400

    Talked about data types

--- day-2/main.py
+++ day-2/main.py
@@ -4,7 +4,14 @@
 "Hello"[4] # the [] allows you to pull out a letter in a string
 
 # Integer
-
 print(123 + 245) # in order to add numbers you remove the quotations
 
-# in order to add separation in number values use 
+# in order to add separation in numbers use underscore instead of commas
+123_123
+
+# Float
+3.14159
+
+# Boolean
+True 
+False
