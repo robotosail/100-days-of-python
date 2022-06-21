50c6c34e19881c5f95575da241afdb5b973cdb3b
Robotosail <d.rojugbokan>
2022-06-20 20:00:16 -0400

    test

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello world\nHello world!\nHello World!") # prints things onto the console # the \n creates a new line within the line of code
+
+# concatinating - we can combine different strings so it can be added to another string at the end. it makes it one string
+print("Hello" + " David")
