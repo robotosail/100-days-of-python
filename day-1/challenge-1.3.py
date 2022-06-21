a22fb1ef14d9369c48e5bd8a4071016913019244
Robotosail <d.rojugbokan>
2022-06-20 21:06:50 -0400

    finished challenge 1.3

---
+++ day-1/challenge-1.3.py
@@ -0,0 +1,7 @@
+# counts the number of characters in a name
+# input("What is your name?")
+print(len(input("What is your name? ")))
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
--- day-1/main.py
+++ day-1/main.py
@@ -1,6 +1,19 @@
-print("Hello world\nHello world!\nHello World!") # prints things onto the console # the \n creates a new line within the line of code
+# prints things onto the console # the \n creates a new line within the line of code
+# print("Hello world\nHello world!\nHello World!")
 
 # concatinating - we can combine different strings so it can be added to another string at the end. it makes it one string
-print("Hello" + " David")
+# print("Hello" + " David")
 
-input("What is your name?")
+# allows user input to be taken
+# input("What is your name?")
+# print("Hello " + input("What is your name?"))
+
+# variables -- holds values that are assigned and need to be used later. it can be reassigned
+# this assigns whatever answer the user puts as an input to the variable name.
+name = "jack"
+# printing out the name variable
+print(name)
+
+name = "David"
+
+print(name)
