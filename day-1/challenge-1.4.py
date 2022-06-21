f378ccb8027aa91f37ddb9ac9fe126a9d4df285d
Robotosail <d.rojugbokan>
2022-06-20 21:31:48 -0400

    Day 1 project complete

---
+++ day-1/challenge-1.4.py
@@ -0,0 +1,11 @@
+a = input("a:")
+b = input("b:")
+
+asquared = a
+bsquared = b
+
+a = bsquared
+b = asquared
+
+print("a = " + a)
+print("b = " + b)
---
+++ day-1/day-1pro.py
@@ -0,0 +1,5 @@
+print("Welcome to Band Name Generator\nAnswer the following questions to get the right band name for  your")
+city = input("What was the city you grew up in? \n")
+pet = input("What is the name of your first pet? \n")
+
+print("Your new band name should be " + city+ " " + pet)
