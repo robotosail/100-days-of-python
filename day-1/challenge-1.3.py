b4378ce2c8efeff7d7a1a3e6632fb75839a75b06
Robotosail <d.rojugbokan>
2022-06-20 21:37:29 -0400

    updated challenge-1.3.py

--- day-1/challenge-1.3.py
+++ day-1/challenge-1.3.py
@@ -1,7 +1,10 @@
 # counts the number of characters in a name
 # input("What is your name?")
+
+# without the use of variables
 print(len(input("What is your name? ")))
 
+# with the use of variables
 name = input("What is your name? ")
 length = len(name)
 print(length)
