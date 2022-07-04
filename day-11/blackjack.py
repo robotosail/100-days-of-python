c193a1ce8e6d266941196994920928025bdf7e98
robotosail <d.rojugbokan@gmail.com>
2022-07-04 16:25:06 -0500

    added art

--- day-11/blackjack.py
+++ day-11/blackjack.py
@@ -1,4 +1,6 @@
 #---------------Black Jack--------------#
+from os import system
+from art import logo
 import random
 
 # function that deals cards randomly
@@ -41,40 +43,47 @@ def compare(user_score, computer_score):
     else:
         return "You lose"
 
+def Game():
+    print(logo)
+    user_cards = []
+    computer_cards = []
 
-user_cards = []
-computer_cards = []
+    # creating a loop to run twice
+    for n in range(2):
+        # appending the new cards to the user card
+        user_cards.append(Deal_card())
+        # appending the new cards to the computer card
+        computer_cards.append(Deal_card())
 
-# creating a loop to run twice
-for n in range(2):
-    # appending the new cards to the user card
-    user_cards.append(Deal_card())
-    # appending the new cards to the computer card
-    computer_cards.append(Deal_card())
+    gameOver = False
+    while not gameOver:
+        # getting hold of the user and the computers score
+        user_score = calc_score(user_cards)
+        computer_score = calc_score(computer_cards)
 
-gameOver = False
-while not gameOver:
-    # getting hold of the user and the computers score
-    user_score = calc_score(user_cards)
-    computer_score = calc_score(computer_cards)
-
-    print(f"Your cards are: {user_cards};\nYour score is {user_score}")
-    print(f"Computer's first card is: {computer_cards[0]}")
-    if user_score == 0 or computer_score == 0 or user_score > 21:
-        gameOver = True
-    else:
-        ask = input(
-            "Do you want to get another card? 'Y' for yes or 'n' for no: ").lower()
-        if ask == "y":
-            user_cards.append(Deal_card())
-        else:
+        print(f"Your cards are: {user_cards};\nYour score is {user_score}")
+        print(f"Computer's first card is: {computer_cards[0]}")
+        if user_score == 0 or computer_score == 0 or user_score > 21:
             gameOver = True
+        else:
+            ask = input(
+                "Do you want to get another card? 'Y' for yes or 'n' for no: ").lower()
+            if ask == "y":
+                user_cards.append(Deal_card())
+            else:
+                gameOver = True
 
-while computer_score != 0 and computer_score < 17:
-    computer_cards.append(Deal_card())
-    computer_score = calc_score(computer_cards)
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(Deal_card())
+        computer_score = calc_score(computer_cards)
 
-print(f"Your final hand: {user_cards}, final score: {user_score}")
-print(
-    f"Computer's final hand: {computer_cards}, final score: {computer_score}")
-print(compare(user_score=user_score, computer_score=computer_score))
+    print(f"Your final hand: {user_cards}, final score: {user_score}")
+    print(
+        f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(user_score=user_score, computer_score=computer_score))
+
+# asking user if they want to play again
+while input("Do you want to play a game of Blackjack? 'Y' for yes or 'N' for no: ") == "y":
+    Game()
+    # clears the screen
+    system("clear")
