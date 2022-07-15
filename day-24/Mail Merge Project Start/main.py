72a68f02c364f9b21d9bec8e417ae9b3161fb567
robotosail <d.rojugbokan@gmail.com>
2022-07-15 12:23:44 -0400

    finished mail merge challenge

--- day-24/Mail Merge Project Start/main.py
+++ day-24/Mail Merge Project Start/main.py
@@ -7,18 +7,47 @@
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-increment = 1
-with open("Input/Letters/starting_letter.txt") as file:
-    contents = file.read()
-    # for letter in contents:
-    # if "name" in contents:
-    #     print("Yes")
-with open("Input/Names/invited_names.txt") as name:
-    names = name.read()
+# the names of the files
+# increment = 1
+# # getting the contents of the starting letter file
+# with open("Input/Letters/starting_letter.txt") as file:
+#     # saving the contents into a variable
+#     contents = file.read()
 
-print(names)
+# # getting the list of names
+# with open("Input/Names/invited_names.txt") as name:
+#     # storing it to a variable then removing whitespaces and \n
+#     names = name.read()
+#     # turn the names into a list after removing the whitespaces and \n
+#     names = names.split()
+
 # for name in names:
-#     with open(f"Output/ReadyToSend/new{increment}.txt", mode="w") as output:
-#         output.write(f"{contents}{contents.replace('name', name)}")
-#     increment += 1
-#     # print("Done")
+#     with open(f"Output/ReadyToSend/{name}'s invitation.txt", mode="w") as output:
+#         # loop through the list of names
+#         # replacing the the string name with the name and assigning it to a variable so it doesn't overwrite
+#         new_contents = contents.replace("[name]", name)
+#         # writing in to the newly created file
+#         output.write(f"{new_contents}")
+#         increment += 1
+
+# or
+# getting the list of names
+with open("Input/Names/invited_names.txt") as name:
+    # storing the names into a list
+    names = name.readlines()
+# getting the contents of the starting letter file
+with open("Input/Letters/starting_letter.txt") as file:
+    # saving the contents into a variable
+    contents = file.read()
+
+# loop through the list of names
+    for name in names:
+        # removing the whitespaces from the names
+        stripped_name = name.strip()
+        # replacing the the placeholder name with the name and assigning it to a variable so it doesn't overwrite
+        new_contents = contents.replace("[name]", stripped_name)
+        print(new_contents)
+
+        with open(f"Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as output:
+            # writing in to the newly created file
+            output.write(f"{new_contents}")
